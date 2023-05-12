6a05dd59ce4550c275c6967564e499e63f86c9f5
NadaKG <124838809+NadaKG@users.noreply.github.com>
2023-05-12 18:32:43 +0300

    Add files via upload

---
+++ W4A5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Assign The Values
+char a = '\0';
+char b = '\n';
+char c = 'd';
+
+// Do Not Edit
+cout << b * c + a << "\n"; // 1000
+}
