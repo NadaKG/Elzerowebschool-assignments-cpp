23cb58930dd4eb4d02e975668cb8e364e8e81ffb
NadaKG <124838809+NadaKG@users.noreply.github.com>
2023-06-17 21:59:14 +0300

    W6A2.cpp

--- W6A2.cpp
+++ W6A2.cpp
@@ -6,18 +6,18 @@ int main()
 {
     int check = 25;
     int nums[]{40, 35, 30, 70, 100};
+    int i = (sizeof(nums)/sizeof(int)) - 2;
 
     if(nums[0] > check )
     {
-        cout << "{" << nums[0] <<"} " << "+" <<" {" << nums[3] << "}" << " = " << nums[0] + nums[3] <<"\n"; 
+        cout << "{" << nums[0] <<"} " << "+" <<" {" << nums[i] << "}" << " = " << nums[0] + nums[i] <<"\n"; 
     }
     if(nums[1] > check) 
     {
-        cout << "{" << nums[1] <<"} " << "+" <<" {" << nums[3] << "}" << " = " << nums[1] + nums[3] <<"\n"; 
+        cout << "{" << nums[1] <<"} " << "+" <<" {" << nums[i] << "}" << " = " << nums[1] + nums[i] <<"\n"; 
     }
     if(nums[2] > check) 
     {
-        cout << "{" << nums[2] <<"} " << "+" <<" {" << nums[3] << "}" << " = " << nums[2] + nums[3]; 
+        cout << "{" << nums[2] <<"} " << "+" <<" {" << nums[i] << "}" << " = " << nums[2] + nums[i]; 
     }
-
 }
