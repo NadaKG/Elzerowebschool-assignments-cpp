a01440923cfb477c6ffef952b3f89d898cf34e62
NadaKG <124838809+NadaKG@users.noreply.github.com>
2023-06-17 19:39:19 +0300

    W3A3.cpp

--- W3A3.cpp
+++ W3A3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-cout << "Maximum Integer Number Is: " << std::numeric_limits<int>::max() << endl; 
+cout << "Maximum Integer Number Is: " << numeric_limits<int>::max() << endl; 
 cout << "Minimum Integer Number Is: " << INT_MIN << endl; 
 cout << "Maximum Short Integer Number Is: " << SHRT_MAX << endl; 
 cout << "Minimum Short Integer Number Is: " << SHRT_MIN << endl; 
