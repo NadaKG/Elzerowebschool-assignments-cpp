bcd80eb7903c95e796de176eb0e0c74ad6ecf384
NadaKG <124838809+NadaKG@users.noreply.github.com>
2023-06-22 13:35:32 +0300

    W7A4.cpp

--- W7A4.cpp
+++ W7A4.cpp
@@ -5,6 +5,8 @@ int main()
 {
     for(int i = 0; i < 19; i += 2)
     {
+        if(i == 10 || i == 12)
+        continue;
         cout << i <<"\n";
     }
     cout <<"===============\n";
@@ -12,7 +14,12 @@ int main()
     int j = 0;
     while (j  < 19)
     {
-        cout << j <<"\n";
-        j += 2;
+       if(j == 10 || j == 12)
+       {
+        j+=2;
+        continue;
+       }
+       cout << j <<"\n";
+       j+=2;
     }
 }
