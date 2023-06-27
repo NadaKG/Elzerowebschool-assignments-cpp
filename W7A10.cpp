d03e3f25b26722a2719a2aed4e851df807a16f55
NadaKG <124838809+NadaKG@users.noreply.github.com>
2023-06-27 14:54:31 +0300

    W7A10.cpp

--- W7A10.cpp
+++ W7A10.cpp
@@ -6,7 +6,7 @@ int main()
     // Friends Array
 string friends[] = {"Ahmed", "Mohamed", "ameer", "Ashraf", "Amany"};
 int aSize = sizeof(friends) / sizeof(friends[0]);
-
+// for loop
 for(int i = 0; i < aSize; i++)
 {
     for(int j = 0; j < 1; j++)
@@ -16,5 +16,16 @@ for(int i = 0; i < aSize; i++)
             cout << friends[i] << "\n";
         }
     }
+}
+
+ //while loop
+int i = 0;
+while (i < size(friends))
+{
+    if (friends[i][0] == 65)
+        {
+            cout << friends[i] << "\n";
+        }
+    i++;
 }
 }
